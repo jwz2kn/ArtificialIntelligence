57858de6d2d1387d16a4689838b293cdde2dd384
jwz2kn <jwz2kn@virginia.edu>
2016-02-25 20:51:41 -0500

    Attempt at error fixing

---
+++ Homework2/.metadata/.plugins/org.eclipse.core.resources/.history/f7/a066a35221dc00151129d933c15da507
@@ -0,0 +1,8 @@
+import world.Robot;
+
+public class RobotTravel extends Robot{
+	@Override
+	public void travelToDestination() {
+		
+	}
+}
--- Homework2/HW2/src/RobotTravelTester.java
+++ Homework2/HW2/src/RobotTravelTester.java
@@ -1,24 +1,24 @@
-import world.World;
+import world.*;
 
 public class RobotTravelTester {
 
 	public static void main(String[] args) {
 		try {
+			//https://en.wikipedia.org/wiki/Pathfinding#Algorithms_used_in_pathfinding
 			/*
-			* Create a world . Pass the input filename first .
-			* Second parameter is whether or not the world is uncertain .
+			* Create a world. Pass the input filename first.
+			* Second parameter is whether or not the world is uncertain.
 			*/
-			World myWorld = new World ("myInputFile.txt ", false );
+			World myWorld = new World ("myInputFile1.txt ", false );
 			/* Create a robot that will run around in myWorld */
-			RobotTravel myRobot = new RobotTravel() ;
-			myRobot.addToWorld ( myWorld );
+			RobotTravel myRobot = new RobotTravel(myWorld.getEndPos()) ;
+			myRobot.addToWorld (myWorld);
 			// Tell the robot to travel to the destination .
 			// You will be implementing this method yourself !
-			myRobot.travelToDestination () ;
-		} catch ( Exception e) {
-			e.printStackTrace () ;
+			myRobot.travelToDestination();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-
 	}
 
 }
