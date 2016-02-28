b5adf5e7b709d10eadf6930e8281c38a0ad5bf3c
jwz2kn <jwz2kn@virginia.edu>
2016-02-27 19:45:44 -0500

    Implemented initial A* with modified Manhattan

--- Homework2/HW2/src/RobotTravelTester.java
+++ Homework2/HW2/src/RobotTravelTester.java
@@ -5,17 +5,20 @@ public class RobotTravelTester {
 	public static void main(String[] args) {
 		try {
 			//https://en.wikipedia.org/wiki/Pathfinding#Algorithms_used_in_pathfinding
+			//https://en.wikipedia.org/wiki/A*_search_algorithm
 			/*
 			* Create a world. Pass the input filename first.
 			* Second parameter is whether or not the world is uncertain.
 			*/
 			boolean uncertainty = false;
 			World myWorld = new World ("myInputFile1.txt", uncertainty );
-			System.out.println("monkey dicks");
+			System.out.println("World Created");
 			/* Create a robot that will run around in myWorld */
 			RobotTravel myRobot 
-				= new RobotTravel(myWorld.getStartPos(), myWorld.getEndPos(), uncertainty);
+				= new RobotTravel(myWorld.getStartPos(), myWorld.getEndPos(), myWorld.getUncertain());
 			myRobot.addToWorld (myWorld);
+			myRobot.setCols(myWorld.numCols());
+			myRobot.setRows(myWorld.numRows());
 			// Tell the robot to travel to the destination .
 			// You will be implementing this method yourself !
 			myRobot.travelToDestination();
