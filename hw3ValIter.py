0a1edafe6df0efa4c27e99f28ac5d0be6f4c6f39
jwz2kn <jwz2kn@virginia.edu>
2016-03-31 16:47:25 -0400

    HW 3 Added Actions and adjustments
    Adjusted actions for wind cases and walls

--- hw3ValIter.py
+++ hw3ValIter.py
@@ -25,24 +25,70 @@ def initialize():
             if i == 3 and j == 6:
                 R[i, j] = 0.0
 
+def U_prime_toString():
+    U_primeStr = ""
+    for i in range(0, 7):
+        for j in range(0, 7):
+            U_primeStr = U_primeStr + str('%.1f' % U_prime[(i,j)]) +" " +"\t"
+        U_primeStr += "\n"
+    return U_primeStr
+
+def actionResultList((i,j)):
+    U_prime_sprime = list()
+    Actions = list()
+    Stay = [i,j]
+    N = [i-1, j]
+    NE = [i-1, j+1]
+    E = [i, j + 1]
+    SE = [i + 1, j + 1]
+    S = [i + 1, j]
+    SW = [i + 1, j - 1]
+    W = [i, j - 1]
+    NW = [i - 1, j - 1]
+
+    Actions.append(Stay);
+    Actions.append(N); Actions.append(NE); Actions.append(E); Actions.append(SE);
+    Actions.append(S); Actions.append(SW); Actions.append(W); Actions.append(NW);
+
+    for a in Actions:
+        if windCase != 1 and (a[1] == 3 or a[1] == 4 or a[1] == 5):
+            a[0] = a[0] - windCase + 1
+        if a[0] > 6:
+            a[0] = 6
+        if a[0] < 0:
+            a[0] = 0
+        if a[1] > 6:
+            a[1] = 6
+        if a[1] < 0:
+            a[1] = 0
+        U_prime_sprime.append(U_prime[tuple(a)])
+
+    return U_prime_sprime
+
 def main():
     global iters, U, d, windCase
     initialize()
-    print U_prime.keys()
     print "Value Iterator."
-    windCase = raw_input("Please type wind case 1, 2, or 3.\n")
-    while d < sigma or iters > MAX_ITERS:
+    windCase = int(raw_input("Please type wind case 1, 2, or 3.\n"))
+    print "Initial U\'"
+    print U_prime_toString()
+    while True:
         U = U_prime
         d = 0
         for s in S:
-            print s
-            U_prime[s] = R[s] # + max action with given constraints and transition model
+            #print s
+            #print s, actionResultList(s) ,max(actionResultList(s))
+            U_prime[s] = R[s] #+ max(actionResultList(s))# + max action with given constraints and transition model
             if abs(U_prime[s] - U[s]) > d:
                 d = abs(U_prime[s] - U[s])
+        print "Iteration", iters
+        print U_prime_toString()
         iters += 1
-
-    # do something with U prime
-    print U_prime
+        if (d < sigma) or (iters > MAX_ITERS):
+            break
+    # # do something with U prime
+    print "U\' after Value Iteration in Case", windCase
+    print U_prime_toString()
 
     return
 
