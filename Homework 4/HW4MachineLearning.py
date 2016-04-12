d6558fb3370ed3862c010066c1b1e20798bca488
jwz2kn <jwz2kn@virginia.edu>
2016-04-11 21:19:30 -0400

    Started HW 4 Recipe Training

---
+++ Homework 4/HW4MachineLearning.py
@@ -0,0 +1,9 @@
+__author__ = 'John Zhang, jwz2kn@virginia.edu', 'Tyler Ealy, tre2ca@virginia.edu'
+
+import json
+import numpy as np
+
+
+# JSON path
+json_data=open().read()
+data = json.loads(json_data)
