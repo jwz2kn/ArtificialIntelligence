3c1e72d1430c6a2ebf2618a59652d15e4c95822e
jwz2kn <jwz2kn@virginia.edu>
2016-04-11 21:56:34 -0400

    Updated json parse
    http://jsonviewer.stack.hu/
    http://stackoverflow.com/questions/2835559/parsing-values-from-a-json-fi
    le-in-python
    http://stackoverflow.com/questions/10973614/convert-json-array-to-python
    -list
    https://docs.python.org/2/library/json.html
    http://stackoverflow.com/questions/17949508/python-read-all-text-file-li
    nes-in-loop
    http://stackoverflow.com/questions/13940272/python-json-loads-returns-it
    ems-prefixing-with-u

--- Homework 4/HW4MachineLearning.py
+++ Homework 4/HW4MachineLearning.py
@@ -3,7 +3,18 @@ __author__ = 'John Zhang, jwz2kn@virginia.edu', 'Tyler Ealy, tre2ca@virginia.edu
 import json
 import numpy as np
 
-
+data = list()
 # JSON path
-json_data=open().read()
-data = json.loads(json_data)
+def main():
+    with open('/Users/John/Desktop/CS 4710/ArtificialIntelligence/Homework 4/training_data.json', 'r') as json_data:
+        for line in json_data:
+            data.append(json.loads(line))
+    #print_training_data(data)
+
+def print_training_data(d):
+    for i in range(len(d)):
+        print(json.dumps(d[i]))
+
+
+if __name__ == "__main__":
+    main()
