8c6cabe456d8f321a7e716ccc1cfeee02d4a6197
jwz2kn <jwz2kn@virginia.edu>
2016-04-24 17:08:44 -0400

    Added helper methods for classifier
    Added helper methods to aid in calculating the probability an
    ingredient occurs in a cuisine.

--- Homework 4/HW4MachineLearning.py
+++ Homework 4/HW4MachineLearning.py
@@ -2,11 +2,16 @@ __author__ = 'John Zhang, jwz2kn@virginia.edu', 'Tyler Ealy, tre7ca@virginia.edu
 
 import json
 import numpy as np
+import math
 
 data = list()
-dummyVarNumerator = 1
+dummyVarNumerator = 1.0
 dummyVarDenominator = 1
 probOfEachLabelDict = {}
+nLogProbOfEachLabel = {}
+probOfEachIngredGivenLabel = {}
+NumOfIngredientsGivenCuisine = {}
+
 # JSON path
 
 def main():
@@ -16,9 +21,14 @@ def main():
             data.append(json.loads(line))
     dummyVarDenominator = len(data)
     fillProbOfEachLabelDict()
-    print(sum(probOfEachLabelDict.values()))
-    print(len(probOfEachLabelDict))
-    print(probOfEachLabelDict)
+    nLogProbOfEachLabel = nLogOfDict(probOfEachLabelDict)
+    fillNumIngredGivenC()
+    # print(sum(probOfEachLabelDict.values()))
+    # print(len(probOfEachLabelDict))
+    # print(probOfEachLabelDict)
+    # print(nLogProbOfEachLabel)
+    # initializeIngredProbList()
+    print(NumOfIngredientsGivenCuisine)
     #print_training_data(data)
     #print(len(data))
 
@@ -26,6 +36,15 @@ def main():
 def print_training_data(d):
     for i in range(len(d)):
         print(json.dumps(d[i]))
+
+
+def nLogOfDict(dict):
+    keys = dict.keys()
+    endDict = {}
+    for k in keys:
+        endDict[k] = math.log(dict[k])
+    return endDict
+
 
 def fillProbOfEachLabelDict():
     denom = len(data)
@@ -47,6 +66,40 @@ def fillProbOfEachLabelDict():
             currentProb = numer/denom
             probOfEachLabelDict[currentLabel] = currentProb
 
+
+def initializeIngredProbList():
+    global probOfEachIngredGivenLabel
+    keys = probOfEachLabelDict.keys()
+    for k in keys:
+        probOfEachIngredGivenLabel[k] = {}
+    print probOfEachIngredGivenLabel
+
+
+def fillIngredProbList():
+    global probOfEachIngredGivenLabel
+    keys = probOfEachLabelDict.keys()
+
+
+def fillNumIngredGivenC():
+    l = len(data)
+    currentIngredientsSum = 0
+    currentLabel = ""
+    for i in range(l):
+        if i == 0:
+            currentLabel = data[i]['cuisine']
+            currentIngredientsSum += len(data[i]['ingredients'])
+            continue
+        if data[i]['cuisine'] == currentLabel:
+            currentIngredientsSum += len(data[i]['ingredients'])
+        elif data[i]['cuisine'] != currentLabel:
+            NumOfIngredientsGivenCuisine[currentLabel] = currentIngredientsSum
+            # reset + count the current thing
+            currentLabel = data[i]['cuisine']
+            currentIngredientsSum = 0
+            currentIngredientsSum += len(data[i]['ingredients'])
+        if i == l - 1:
+            currentIngredientsSum += len(data[i]['ingredients'])
+            NumOfIngredientsGivenCuisine[currentLabel] = currentIngredientsSum
 
 if __name__ == "__main__":
     main()
